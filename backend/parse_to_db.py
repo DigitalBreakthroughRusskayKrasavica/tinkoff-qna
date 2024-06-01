4739f66fb91c939b0ab546de00debf2486cdb64b
akiko23 <usksoako@gmail.com>
2024-06-01 11:50:14 +0300

    [update] add migration which fills database with data from dataset

--- backend/parse_to_db.py
+++ backend/parse_to_db.py
@@ -1,72 +1,57 @@
 import csv
 import json
 
+from sqlalchemy import text
 from sqlalchemy.engine import Engine, create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.exc import IntegrityError
 
 from src.tinkoff_qna.lms.rubert.get_answer import BertModel
-from src.tinkoff_qna.models import QuestionAnswer
+
+def parse_to_db():
+    def create_db_engine(db_uri: str) -> Engine:
+        engine_options = {
+            "echo": False,
+            "pool_size": 15,
+            "max_overflow": 15,
+        }
+        return create_engine(db_uri, **engine_options)
 
 
-def create_db_engine(db_uri: str) -> Engine:
-    engine_options = {
-        "echo": False,
-        "pool_size": 15,
-        "max_overflow": 15,
-    }
-    return create_engine(db_uri, **engine_options)
+    def create_session_maker(engine: Engine) -> sessionmaker[Session]:
+        return sessionmaker(engine, autoflush=True, expire_on_commit=False)
 
 
-def create_session_maker(engine: Engine) -> sessionmaker[Session]:
-    return sessionmaker(engine, autoflush=True, expire_on_commit=False)
+    DB_URI = "postgresql://postgres:postgres@localhost:5432/postgres"
 
+    engine = create_db_engine(DB_URI)
+    session_factory = create_session_maker(engine)
 
-DB_URI = "postgresql://postgres:postgres@localhost:5432/postgres"
+    model_facade = BertModel(DB_URI)
 
-engine = create_db_engine(DB_URI)
-session_factory = create_session_maker(engine)
+    with open(
+            "./train_Tinkoff/dataset.json",
+            'r', encoding='utf-8') as f:
+        data = json.load(f)['data']
+        with session_factory() as session:
+            for row in data:
+                if len(row['description']) <= 1:
+                    continue
+                
+                emb = model_facade.generate_embeddings([row['title']])
+                session.execute(
+                    statement=text('INSERT INTO question_answer (question, product, source, url, type, embedding, parent_title, parent_url, answer) VALUES (:question, :product, :source, :url, :type, :embedding, :parent_title, :parent_url, :answer)'), 
+                    params={
+                        'question': row['title'],
+                        'product': row['product'],
+                        'source': row['source'],
+                        'url': row['url'],
+                        'type': row['type'],
+                        'embedding': emb,
+                        'parent_title': row['parent_title'],
+                        'parent_url': row['parent_url'],
+                        'answer': row['description']
+                    },
+                )
 
-model_facade = BertModel(DB_URI)
-
-# with open(
-#         "./src/curator_support/lms/rasa/answer_class.csv",
-#         'r', encoding='utf-8') as f:
-#     reader = csv.DictReader(f)
-#     with session_factory() as session:
-#         for row in reader:
-#             session.add(
-#                 Answer(
-#                     id=row['answer_class'],
-#                     answer=row['Answer'],
-#                 )
-#             )
-#         try:
-#             session.commit()
-#         except IntegrityError as e:
-#             print(e)
-
-
-
-with open(
-        "./train_Tinkoff/dataset.json",
-        'r', encoding='utf-8') as f:
-    data = json.load(f)['data']
-    with session_factory() as session:
-        for row in data:
-            emb = model_facade.generate_embeddings([row['title']])
-            session.add(
-                QuestionAnswer(
-                    question=row['title'],
-                    product=row['product'],
-                    source=row['source'],
-                    url=row['url'],
-                    type_=row['type'],
-                    embedding=emb,
-                    parent_title=row['parent_title'],
-                    parent_url=row['parent_url'],
-                    answer=row['description'],
-                ),
-            )
-
-        session.commit()
+            session.commit()
